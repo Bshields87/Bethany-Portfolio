fa7c83f5bb76c9b9269b60429cd4ab3dce37fd7f
Bshields87 <shields.bethany@gmail.com>
2019-03-21 13:03:59 -0700

    counter clockwise

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -45,3 +45,69 @@ var triangle1 = [
     triangle3, 
    triangle3Timing
   )
+
+  var triangle4 = [
+    { transform: 'rotate(0) translate3D(-30%, -30%, 0', color: '#000' }, 
+    { color: '#431236', offset: 0.6},
+    { transform: 'rotate(360deg) translate3D(-30%, -30%, 0)', color: '#000' }
+  ];
+  
+  var triangle4Timing = {
+    duration: 14000,
+    iterations: Infinity
+  }
+  
+  document.getElementById("triangle4").animate(
+    triangle4, 
+   triangle4Timing
+  )
+
+  var triangle5 = [
+    { transform: 'rotate(0) translate3D(-30%, -30%, 0', color: '#000' }, 
+    { color: '#431236', offset: 0.6},
+    { transform: 'rotate(-360deg) translate3D(-30%, -30%, 0)', color: '#000' }
+  ];
+  
+  var triangle5Timing = {
+    duration: 12000,
+    iterations: Infinity
+  }
+  
+  document.getElementById("triangle5").animate(
+    triangle5, 
+   triangle5Timing
+  )
+
+  var triangle6 = [
+    { transform: 'rotate(0) translate3D(-30%, -30%, 0', color: '#000' }, 
+    { color: '#431236', offset: 0.6},
+    { transform: 'rotate(360deg) translate3D(-30%, -30%, 0)', color: '#000' }
+  ];
+  
+  var triangle6Timing = {
+    duration: 10000,
+    iterations: Infinity
+  }
+  
+  document.getElementById("triangle6").animate(
+    triangle6, 
+   triangle6Timing
+  )
+
+
+  var triangle7 = [
+    { transform: 'rotate(0) translate3D(-30%, -30%, 0', color: '#000' }, 
+    { color: '#431236', offset: 0.6},
+    { transform: 'rotate(-360deg) translate3D(-30%, -30%, 0)', color: '#000' }
+  ];
+  
+  var triangle7Timing = {
+    duration: 10000,
+    iterations: Infinity
+  }
+  
+  document.getElementById("triangle7").animate(
+    triangle7, 
+   triangle7Timing
+  )
+  
